d89fa274294e7d8d6e542c95108b31feac0c26bc
Darwin_N19 <darwinccn25@gmail.com>
2024-05-16 11:04:00 -0500

    added morgan middleware for requests info

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express"
+import morgan from "morgan"
 import { env } from "~/env"
 
 const app = express()
 const PORT = env.PORT
+
+app.use(morgan("dev"))
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!")
