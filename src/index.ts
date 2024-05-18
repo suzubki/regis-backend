98a731c45f5249d8df0ff1b70d205ca35a8789bf
Darwin_N19 <darwinccn25@gmail.com>
2024-05-18 13:45:43 -0500

    added jsonwebtoken and enabled express json

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express, { type Request, type Response } from "express"
 import morgan from "morgan"
 import { env } from "~/env"
-import routes from "./routes"
+import routes from "./server/routes"
 
 const app = express()
 const PORT = env.PORT
+
+app.use(express.json())
 
 app.use(morgan("dev"))
 
 app.use("/", routes)
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   res.send("Hello, TypeScript with Express!")
 })
 
