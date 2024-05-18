914bff7f9ada9c5d31177e73e0c80c5d343aca2f
Darwin_N19 <darwinccn25@gmail.com>
2024-05-18 18:31:13 -0500

    added password and timestamp fields

--- src/data/model/schema.ts
+++ src/data/model/schema.ts
@@ -1,10 +1,14 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core"
+import { pgTable, serial, text, timestamp } from "drizzle-orm/pg-core"
 
 export const usersModel = pgTable("users", {
   id: serial("id").primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
-  picture_url: text("picture_url").notNull(),
+  picture_url: text("picture_url"),
+  password: text("password").notNull(),
+
+  created_at: timestamp("created_at").defaultNow(),
+  updated_at: timestamp("updated_at").defaultNow(),
 })
 
 export type InsertUser = typeof usersModel.$inferInsert
