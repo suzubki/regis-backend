69f293ac3634660a59b672133057bdbeeab84e59
Darwin_N19 <darwinccn25@gmail.com>
2024-05-18 18:07:18 -0500

    added return to mantain convention

--- src/server/validations/auth.validation.ts
+++ src/server/validations/auth.validation.ts
@@ -13,16 +13,16 @@ export const validateData = (schema: z.ZodObject<any, any>) => {
     try {
       schema.parse(req.body)
 
-      next()
+      return next()
     } catch (error) {
       if (error instanceof ZodError) {
         const errorMessages = error.errors.map((issue: any) => ({
           message: `${issue.path.join(".")} is ${issue.message}`,
         }))
 
-        res.status(400).json({ error: "Invalid data", details: errorMessages })
+        return res.status(400).json({ error: "Invalid data", details: errorMessages })
       } else {
-        res.status(500).json({ error: "Internal Server Error" })
+        return res.status(500).json({ error: "Internal Server Error" })
       }
     }
   }
