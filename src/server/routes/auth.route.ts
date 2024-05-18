05a71197aee7f438eed1f743446535d86a2bebd3
Darwin_N19 <darwinccn25@gmail.com>
2024-05-18 18:34:27 -0500

    fixed path

--- src/server/routes/auth.route.ts
+++ src/server/routes/auth.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express"
 import AuthController from "~/server/controllers/auth.controller"
-import { createUserSchema, validateData } from "~/server/validations/auth.validation"
+import { validateData } from "~/server/utils/common"
+import { createUserSchema, loginUserSchema } from "~/server/validations/auth.validation"
 
 const router = Router()
 
-router.post("/login", AuthController.login)
+router.post("/login", validateData(loginUserSchema), AuthController.login)
 router.post("/signup", validateData(createUserSchema), AuthController.signUp)
 
 export { router as authRouter }
