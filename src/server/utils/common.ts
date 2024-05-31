7c66b004c0e27d3ed93f569a26ec735328112ec6
Darwin_N19 <darwinccn25@gmail.com>
2024-05-31 14:05:25 -0500

    added sign functions using jwt

--- src/server/utils/common.ts
+++ src/server/utils/common.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import bcrypt from "bcrypt"
 import type { NextFunction, Request, Response } from "express"
+import jwt from "jsonwebtoken"
 import { z, ZodError } from "zod"
 import { env } from "~/env"
 
@@ -24,15 +25,55 @@ export const validateData = (schema: z.ZodObject<any, any>) => {
   }
 }
 
+// ----------------------------
 export const crypt = {
   encrypt: encryptString,
   compare: compareString,
 }
 
+/**
+ * Encrypts a string using SALTS_ROUNDS
+ * @param str
+ * @returns Promise string
+ */
 async function encryptString(str: string) {
   return await bcrypt.hash(str, Number(env.SALT_ROUNDS))
 }
 
+/**
+ * Compares a string with a hash
+ * @param str
+ * @param hash
+ * @returns Promise boolean
+ */
 async function compareString(str: string, hash: string) {
   return await bcrypt.compare(str, hash)
+}
+
+// ----------------------------
+export const signToken = {
+  accessToken: signAccessToken,
+  refreshToken: signRefreshToken,
+}
+
+/**
+ * We are using the user email, id, name and picture_url to sign the access token
+ * @param user
+ * @returns Promise string
+ */
+function signAccessToken(user: Record<string, any>) {
+  return jwt.sign({ email: user.email, id: user.id, name: user.name, picture_url: user.picture_url }, process.env.JWT_SECRET as string, {
+    expiresIn: "1h",
+  })
+}
+
+/**
+ * We are using the user id to sign the refresh token
+ * @param user, user object
+ * @returns Promise string
+ */
+function signRefreshToken(user: Record<string, any>) {
+  return jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, {
+    expiresIn: "31d",
+  })
 }
